e00b5f488d0af7eff97c61c58785482aff3f1e07
Aziz Shahmardanli <aziz.shahmardan@gmail.com>
2024-02-19 19:33:24 +0400

    initialize npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "To Do",
+      template: "./src/template.html",
+
+      filename: "./index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      }
+    ]
+  },
+  devtool: "inline-source-map"
+};
